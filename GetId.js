dca1dbcdafd9f3ff86c0c859c1d82b291b844e43
Mayur shimpi <54832293+MAYU-28@users.noreply.github.com>
2022-01-30 19:47:28 +0530

    Add files via upload

---
+++ GetId.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function passid() {
+    const testMe = (event, value) => {
+        console.log(value);
+      };
+  return <>
+  {fltr.map((result, id) => (
+          <>
+            <ul style={{ margin: "0.1rem" }}>
+              <li key={result.id}>
+                <a
+                  href="#"
+                  onClick={(event) => testMe(event, result.id)} showid={result.id}
+                
+                >
+
+                  <h5>{result.employee}</h5>
+                </a>
+              </li>
+            </ul>
+          </>
+        ))}
+  </>;
+}
+
+export default passid;
